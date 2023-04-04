f2a6f8c74e1bd6999740907edf6a6b74b57130b3
Tich123 <mashavatich@gmail.com>
2023-04-04 11:15:01 -0700

    9. Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,61 +1,40 @@
-#include <stdio.h>
+#include "lists.h"
 #include <stdlib.h>
 
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * 
- */
-typedef struct listint_s
-{
-    int n;
-    struct listint_s *next;
-} listint_t;
+ * insert_nodeint_at_index - Insert a new node at a given positiion.
+ * @head: First node address.
+ * @idx: Position of the new node to be inserted in.
+ * @n: Data of the new node.
+ * Return: Address of the new node.
+ **/
 
-/**
- * insert_nodeint_at_index - insert node at a given position
- * @head: head of list
- * @idx: index of the node to insert
- * @n: value to insert in the new node
- *
- * Return: address of new node, NULL if it failed
- */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-    listint_t *new_node, *temp;
-    unsigned int i;
+	listint_t *new_node, *temp;
+	unsigned int i = 0;
 
-    if (!head)
-        return (NULL);
-
-    new_node = malloc(sizeof(listint_t));
-    if (!new_node)
-        return (NULL);
-
-    new_node->n = n;
-
-    if (!idx)
-    {
-        new_node->next = *head;
-        *head = new_node;
-        return (new_node);
-    }
-
-    temp = *head;
-    for (i = 0; i < idx - 1 && temp; i++)
-        temp = temp->next;
-
-    if (!temp)
-    {
-        free(new_node);
-        return (NULL);
-    }
-
-    new_node->next = temp->next;
-    temp->next = new_node;
-
-    return (new_node);
+	if (*head == NULL && idx != 0)
+		return (NULL);
+	if (idx != 0)
+	{
+	temp = *head;
+	for (; i < idx - 1 && temp != NULL; i++)
+		temp = temp->next;
+	if (temp == NULL)
+		return (NULL);
+	}
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+	new_node->next = temp->next;
+	temp->next = new_node;
+	return (new_node);
 }
