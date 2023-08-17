68e1655883a623e5c2e9f73d65fc1b3fa09372d2
Tich123 <mashavatich@gmail.com>
2023-08-17 11:34:30 -0700

    python

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -3,6 +3,7 @@
 /**
  * dlistint_len - returns the number of elements in
  * a double linked list
+ *
  * @h: head of the list
  * Return: the number of nodes
  */
@@ -25,4 +26,4 @@ size_t dlistint_len(const dlistint_t *h)
 	}
 
 	return (count);
-}i
+}
